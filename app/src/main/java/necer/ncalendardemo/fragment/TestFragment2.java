5f844827281cfe9d859d0c26004d1733b23b6ce1
necer <619008099@qq.com>
2018-05-09 09:43:41 +0800

    测试

--- app/src/main/java/necer/ncalendardemo/fragment/TestFragment2.java
+++ app/src/main/java/necer/ncalendardemo/fragment/TestFragment2.java
@@ -3,11 +3,14 @@ package necer.ncalendardemo.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.necer.ncalendar.calendar.MonthCalendar;
+import com.necer.ncalendar.calendar.NCalendar;
 import com.necer.ncalendar.listener.OnMonthCalendarChangedListener;
 import com.necer.ncalendar.utils.MyLog;
 
@@ -15,6 +18,7 @@ import org.joda.time.DateTime;
 import org.joda.time.LocalDate;
 
 import necer.ncalendardemo.R;
+import necer.ncalendardemo.adapter.AAAdapter;
 
 /**
  * Created by necer on 2018/2/5.
@@ -22,22 +26,33 @@ import necer.ncalendardemo.R;
 
 public class TestFragment2 extends Fragment {
 
-    MonthCalendar monthCalendar;
+  //  MonthCalendar monthCalendar;
+
+
+    private NCalendar ncalendar;
+    private RecyclerView recyclerView;
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View inflate = inflater.inflate(R.layout.fragment_2, null);
-        monthCalendar = (MonthCalendar) inflate.findViewById(R.id.monthcalendar);
+       /* monthCalendar = (MonthCalendar) inflate.findViewById(R.id.monthcalendar);
 
         monthCalendar.setOnMonthCalendarChangedListener(new OnMonthCalendarChangedListener() {
             @Override
             public void onMonthCalendarChanged(LocalDate dateTime) {
                 MyLog.d("TestFragment2::;"+dateTime.toString());
             }
-        });
+        });*/
 
+        ncalendar = (NCalendar) inflate.findViewById(R.id.ncalendarrrr);
+        recyclerView = (RecyclerView) inflate.findViewById(R.id.recyclerView);
 
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        AAAdapter aaAdapter = new AAAdapter(getContext());
+        recyclerView.setAdapter(aaAdapter);
 
         return inflate;
 
@@ -49,7 +64,7 @@ public class TestFragment2 extends Fragment {
         super.onHiddenChanged(hidden);
 
         if (!hidden) {
-            monthCalendar.toToday();
+            ncalendar.toToday();
         }
     }
 }
