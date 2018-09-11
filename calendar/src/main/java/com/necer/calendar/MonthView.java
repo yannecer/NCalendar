87cf2520dc46eadf2e4b50ca97eccab7b32b8d3d
闫彬彬 <619008099@qq.com>
2018-09-11 13:51:08 +0800

    新建calendar

---
+++ calendar/src/main/java/com/necer/calendar/MonthView.java
@@ -0,0 +1,19 @@
+package com.necer.calendar;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by necer on 2018/9/11.
+ */
+public class MonthView extends View {
+    public MonthView(Context context) {
+        super(context);
+    }
+
+    public MonthView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
